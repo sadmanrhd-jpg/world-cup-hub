c22eaed7b506c59213e901fd05d32f8e9e2f5fe8
sadmanrhd-jpg <sadmanrhd@gmail.com>
2026-06-28 12:43:51 +0600

    Updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Layout from "./components/Layout";
+import TournamentFixtureSync from "./components/TournamentFixtureSync";
 import Home from "./pages/Home";
 import Groups from "./pages/Groups";
-import Table from "./pages/Table";
 import Teams from "./pages/Teams";
 import TeamPage from "./pages/TeamPage";
 import Fixtures from "./pages/Fixtures";
@@ -24,11 +24,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <TournamentFixtureSync />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/groups" element={<Groups />} />
-            <Route path="/table" element={<Table />} />
+            <Route path="/table" element={<Navigate to="/groups" replace />} />
             <Route path="/teams" element={<Teams />} />
             <Route path="/teams/:slug" element={<TeamPage />} />
             <Route path="/fixtures" element={<Fixtures />} />
